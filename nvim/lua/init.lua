a928fa12761a2b83c03f1457fa74b0583385fbdd
liyong <hungrybirder@gmail.com>
2021-04-07 16:23:29 +0800

    Add hb.lua for my testing.

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -4,3 +4,6 @@ P = function(...) print(vim.inspect(...)) end
 require('module/treesitter')
 require('module/lsp')
 require('module/telescope')
+
+-- hb is a test.
+require('module/hb')
---
+++ nvim/lua/module/hb.lua
@@ -0,0 +1,9 @@
+
+M = {}
+
+function M.hello()
+  print("hello hb")
+end
+
+
+return M
