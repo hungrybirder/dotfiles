be81c4a061e0dcb07538d3584e84da96819048a2
liyong <hungrybirder@gmail.com>
2021-04-27 11:53:33 +0800

    update lsp.lua

--- nvim/lua/module/lsp.lua
+++ nvim/lua/module/lsp.lua
@@ -169,7 +169,7 @@ local on_attach = function(client, bufnr)
     customize_lsp_label = customize_lsp_label,
   })
 
-  lsp_status.on_attach(client, bufnr)
+  lsp_status.on_attach(client)
   -- vim.api.nvim_command('autocmd CursorHold <buffer> lua vim.lsp.diagnostic.show_line_diagnostics()')
   -- if vim.api.nvim_buf_get_option(0, 'filetype') == 'rust' then
   --   vim.api.nvim_command('autocmd CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost *.rs lua require"lsp_extensions".inlay_hints {prefix=" » ", highlight = "Comment", enabled = {"TypeHint","ChainingHint", "ParameterHint"}}')
