7158ffb8ef0abb04ee638cb5e729378eb445f521
liyong <hungrybirder@gmail.com>
2021-05-06 14:02:38 +0800

    config bubbly color.

--- nvim/lua/module/bubbly.lua
+++ nvim/lua/module/bubbly.lua
@@ -17,3 +17,64 @@ vim.g.bubbly_statusline = {
   'filetype',
   -- 'total_buffer_number',
 }
+
+vim.g.bubbly_colors = {
+  default = 'red',
+
+  mode = {
+    normal = 'green', -- uses by default 'background' as the foreground color.
+    insert = 'blue',
+    visual = 'red',
+    visualblock = 'red',
+    command = 'red',
+    terminal = 'blue',
+    replace = 'yellow',
+    default = 'white'
+  },
+  path = {
+    readonly = { background = 'lightgrey', foreground = 'foreground' },
+    unmodifiable = { background = 'darkgrey', foreground = 'foreground' },
+    path = 'white',
+    modified = { background = 'lightgrey', foreground = 'foreground' },
+  },
+  -- branch = 'purple',
+  branch = 'cyan',
+  signify = {
+    added = 'green',
+    modified = 'blue',
+    removed = 'red',
+  },
+  paste = 'red',
+  coc = {
+    error = 'red',
+    warning = 'yellow',
+    status = { background = 'lightgrey', foreground = 'foreground' },
+  },
+  builtinlsp = {
+    diagnostic_count = {
+      error = 'red',
+      warning = 'yellow',
+    },
+    current_function = 'purple',
+  },
+  filetype = 'blue',
+  progress = {
+    rowandcol = { background = 'darkgrey', foreground = 'foreground' },
+    percentage = { background = 'darkgrey', foreground = 'foreground' },
+  },
+  tabline = {
+    active = 'blue',
+    inactive = 'white',
+    close = 'darkgrey',
+  },
+  total_buffer_number = 'cyan',
+  lsp_status = {
+    messages = 'white',
+    diagnostics = {
+      error = 'red',
+      warning = 'yellow',
+      hint = 'white',
+      info = 'blue',
+    },
+  },
+}
