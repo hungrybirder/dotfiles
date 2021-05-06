36f628d17bcd1ae79e604a934547ef8ed5a89e2c
liyong <hungrybirder@gmail.com>
2021-05-06 23:01:07 +0800

    config vim.g.bubbly_palette

--- nvim/lua/module/bubbly.lua
+++ nvim/lua/module/bubbly.lua
@@ -10,15 +10,15 @@ vim.g.bubbly_statusline = {
   'divisor',
 
   'lsp_status.messages',
-  'builtinlsp.current_function',
   'lsp_status.diagnostics',
+  'builtinlsp.current_function',
   -- 'builtinlsp.diagnostic_count',
   'progress',
   'filetype',
   -- 'total_buffer_number',
 }
 
-disable_bubbly_filetypes = { 'NvimTree', 'Outline', }
+disable_bubbly_filetypes = { 'NvimTree', 'Outline', 'vim-plug', }
 
 vim.g.bubbly_filter = {
   mode = disable_bubbly_filetypes,
@@ -94,4 +94,19 @@ vim.g.bubbly_colors = {
 vim.g.bubbly_inactive_color = {
   background = 'darkgrey',
   foreground = 'foreground',
+}
+
+vim.g.bubbly_palette = {
+  background = "#34343c",
+  foreground = "#c5cdd9",
+  black = "#3e4249",
+  red = "#ec7279",
+  green = "#a0c980",
+  yellow = "#deb974",
+  blue = "#6cb6eb",
+  purple = "#d38aea",
+  cyan = "#5dbbc1",
+  white = "#c5cdd9",
+  lightgrey = "#57595e",
+  darkgrey = "#404247",
 }
