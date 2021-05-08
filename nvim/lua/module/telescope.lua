8d6ea3bb2220ebad791641666fa95333bb9168e4
liyong <hungrybirder@gmail.com>
2021-05-08 14:37:06 +0800

    use telescope-fzf-native

--- nvim/lua/module/telescope.lua
+++ nvim/lua/module/telescope.lua
@@ -31,15 +31,18 @@ require('telescope').setup{
     qflist_previewer = previewers.vim_buffer_qflist.new,
   },
   extensions = {
-      fzy_native = {
-          override_generic_sorter = false,
-          override_file_sorter = true,
-      },
-      fzf_writer = {
-          use_highlighter = true,
-      },
+    fzf = {
+      override_generic_sorter = false, -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+    -- fzf_writer = {
+    --     use_highlighter = true,
+    -- },
   }
 }
-require('telescope').load_extension('fzy_native')
+-- require('telescope').load_extension('fzy_native')
+require('telescope').load_extension('fzf')
 
 -- require'telescope.builtin'.symbol{ sources = {'emoji'} }
