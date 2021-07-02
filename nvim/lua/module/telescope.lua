8878546d35dc6ff0294c40df55844f85272c8de7
liyong <hungrybirder@gmail.com>
2021-07-02 08:54:21 +0800

    Update telescope.lua

--- nvim/lua/module/telescope.lua
+++ nvim/lua/module/telescope.lua
@@ -1,8 +1,8 @@
 local actions = require('telescope.actions')
 local previewers = require('telescope.previewers')
+
 require('telescope').setup{
   defaults = {
-    -- file_sorter = require('telescope.sorters').get_fzy_sorter,
     mappings = {
       i = {
         ["<c-j>"] = actions.move_selection_next,
@@ -16,38 +16,58 @@ require('telescope').setup{
         ["<c-q>"] = actions.send_to_qflist,
       },
     },
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case'
+    },
+    prompt_prefix = "> ",
+    selection_caret = "> ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "descending",
     layout_strategy = "flex",
     scroll_strategy = 'cycle',
-    winblend = 5,
-    layout_defaults = {
+    layout_config = {
       horizontal = {
-        width_padding = 0.1,
-        height_padding = 0.1,
-        preview_width = 0.55,
+        mirror = false,
+      },
+      vertical = {
+        mirror = false,
       },
     },
+    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+    file_ignore_patterns = {},
+    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+    shorten_path = true,
+    winblend = 5,
+    border = {},
+    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+    color_devicons = true,
+    use_less = true,
+    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
     file_previewer = previewers.vim_buffer_cat.new,
     grep_previewer = previewers.vim_buffer_vimgrep.new,
     qflist_previewer = previewers.vim_buffer_qflist.new,
+
+    -- Developer configurations: Not meant for general override
+    buffer_previewer_maker = previewers.buffer_previewer_maker
   },
   extensions = {
-    -- fzf = {
-    --   override_generic_sorter = false, -- override the generic sorter
-    --   override_file_sorter = true,     -- override the file sorter
-    --   case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-    --                                    -- the default case_mode is "smart_case"
-    -- },
     fzy_native = {
       override_generic_sorter = false,
       override_file_sorter = true,
     },
-    -- fzf_writer = {
-    --     use_highlighter = true,
-    -- },
   }
 }
+
+
 require('telescope').load_extension('fzy_native')
--- require('telescope').load_extension('fzf')
 
 -- require'telescope.builtin'.symbol{ sources = {'emoji'} }
 require('telescope').load_extension('dap')
