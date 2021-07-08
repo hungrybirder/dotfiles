e362666a47e3ae9d3a6ed14f8413253ab71d5357
liyong <hungrybirder@gmail.com>
2021-07-08 18:28:53 +0800

    Update telescope.lua
    path_display = {"absolute"}

--- nvim/lua/module/telescope.lua
+++ nvim/lua/module/telescope.lua
@@ -44,7 +44,9 @@ require('telescope').setup{
     file_sorter =  require'telescope.sorters'.get_fuzzy_file,
     file_ignore_patterns = {},
     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-    shorten_path = true,
+    path_display = {
+      "absolute",
+    },
     winblend = 5,
     border = {},
     borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
