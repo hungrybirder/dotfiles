f517dbbd583f3b2c49e781203194addc277e80ea
liyong <hungrybirder@gmail.com>
2021-10-17 12:05:56 +0800

    Remove unsed variables.

--- nvim/lua/module/nvim-cmp.lua
+++ nvim/lua/module/nvim-cmp.lua
@@ -14,7 +14,6 @@ local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
-local lspkind = require('lspkind')
 local cmp = require'cmp'
 cmp.setup{
   snippet = {
