a561515619eba1f085e6b4069bb103ce2826ea2b
liyong <hungrybirder@gmail.com>
2021-10-20 16:41:53 +0800

    disable_netrw = false

--- nvim/lua/module/nvim-tree.lua
+++ nvim/lua/module/nvim-tree.lua
@@ -34,7 +34,7 @@ vim.g.nvim_tree_special_files = {['README.md'] = true, Makefile = true, MAKEFILE
 vim.g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1}
 
 require'nvim-tree'.setup {
-    disable_netrw = true,
+    disable_netrw = false,
     hijack_netrw = true,
     open_on_setup = false,
     ignore_ft_on_setup = {},
