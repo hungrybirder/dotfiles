e60e1aedcf41d5c578f0fdcc6c48a46393a534e4
liyong <hungrybirder@gmail.com>
2021-11-23 09:14:45 +0800

    update bqf config

--- nvim/lua/module/nvim-bqf.lua
+++ nvim/lua/module/nvim-bqf.lua
@@ -1,16 +1,10 @@
 require('bqf').setup({
     auto_enable = true,
-    preview = {
-        win_height = 12,
-        win_vheight = 12,
-        delay_syntax = 80,
-        border_chars = {'┃', '┃', '━', '━', '┏', '┓', '┗', '┛', '█'}
-    },
-    filter = {fzf = {extra_opts = {'--bind', 'ctrl-o:toggle-all', '--prompt', '> '}}}
+    preview = { win_height = 12, win_vheight = 12, delay_syntax = 80 },
+    filter = { fzf = { extra_opts = { '--bind', 'ctrl-o:toggle-all', '--prompt', '> ' } } }
 })
 
-vim.api.nvim_exec([[
-  hi BqfPreviewBorder guifg=#45a14f ctermfg=71
-  hi link BqfPreviewRange Search
-]], false)
-
+vim.cmd([[
+    hi BqfPreviewBorder guifg=#a0c980 ctermfg=71
+    hi link BqfPreviewRange Search
+]])
