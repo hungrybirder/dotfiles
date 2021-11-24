925fc27b33b57ce863c16eb2ad9f749fbe6432cd
liyong <hungrybirder@gmail.com>
2021-11-24 18:37:44 +0800

    use folder

--- nvim/lua/module/treesitter.lua
+++ nvim/lua/module/treesitter.lua
@@ -1,20 +1,20 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"lua", "python", "go", "c", "cpp", "java", "typescript", "javascript"},
-    indent = {enable = true},
-    highlight = {enable = true},
+    ensure_installed = { "lua", "python", "go", "c", "cpp", "java", "typescript", "javascript" },
+    indent = { enable = true },
+    highlight = { enable = true },
     incremental_selection = {
         enable = true,
         keymaps = {
             init_selection = 'gnn',
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm"
+            scope_incremental = '<CR>',
+            node_incremental = '<TAB>',
+            node_decremental = '<S-TAB>'
         }
     },
     refactor = {
-        highlight_definitions = {enable = false},
-        highlight_current_scope = {enable = false},
-        smart_rename = {enable = true, keymaps = {smart_rename = "grr"}},
+        highlight_definitions = { enable = false },
+        highlight_current_scope = { enable = false },
+        smart_rename = { enable = true, keymaps = { smart_rename = "grr" } },
         navigation = {
             enable = true,
             keymaps = {
@@ -49,13 +49,13 @@ require'nvim-treesitter.configs'.setup {
                 ["ad"] = "@comment.outer"
             }
         },
-        lsp_interop = {enable = true, peek_definition_code = {["df"] = "@function.outer", ["dF"] = "@class.outer"}},
+        lsp_interop = { enable = true, peek_definition_code = { ["df"] = "@function.outer", ["dF"] = "@class.outer" } },
         move = {
             enable = true,
-            goto_next_start = {["]m"] = "@function.outer", ["]]"] = "@class.outer"},
-            goto_next_end = {["]M"] = "@function.outer", ["]["] = "@class.outer"},
-            goto_previous_start = {["[m"] = "@function.outer", ["[["] = "@class.outer"},
-            goto_previous_end = {["[M"] = "@function.outer", ["[]"] = "@class.outer"}
+            goto_next_start = { ["]m"] = "@function.outer", ["]]"] = "@class.outer" },
+            goto_next_end = { ["]M"] = "@function.outer", ["]["] = "@class.outer" },
+            goto_previous_start = { ["[m"] = "@function.outer", ["[["] = "@class.outer" },
+            goto_previous_end = { ["[M"] = "@function.outer", ["[]"] = "@class.outer" }
         }
     },
     playground = {
@@ -64,13 +64,15 @@ require'nvim-treesitter.configs'.setup {
         updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
         persist_queries = false -- Whether the query persists across vim sessions
     },
-    query_linter = {enable = true, use_virtual_text = true, lint_events = {"BufWrite", "CursorHold"}},
-    matchup = {enable = true},
-    autopairs = {enable = true}
+    query_linter = { enable = true, use_virtual_text = true, lint_events = { "BufWrite", "CursorHold" } },
+    matchup = { enable = true },
+    autopairs = { enable = true }
 }
 
 -- TODO: learn vim folder
--- local parsers = require'nvim-treesitter.parsers'
--- local configs = require'nvim-treesitter.parsers'.get_parser_configs()
--- local ft_str = table.concat(vim.tbl_map(function(ft) return configs[ft].filetype or ft end, parsers.available_parsers()), ',')
--- vim.cmd('autocmd Filetype ' .. ft_str .. ' setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr()')
+local parsers = require 'nvim-treesitter.parsers'
+local configs = require'nvim-treesitter.parsers'.get_parser_configs()
+local ft_str = table.concat(vim.tbl_map(function(ft)
+    return configs[ft].filetype or ft
+end, parsers.available_parsers()), ',')
+vim.cmd('autocmd Filetype ' .. ft_str .. ' setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr()')
