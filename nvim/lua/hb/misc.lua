27a24268384c1e13ba2690e7d3b31b2807c1de55
liyong <hungrybirder@gmail.com>
2022-01-01 12:03:18 +0800

    Update nvim-bpf fzf action_for

--- nvim/lua/hb/misc.lua
+++ nvim/lua/hb/misc.lua
@@ -66,7 +66,10 @@ require"neoscroll.config".set_mappings(t)
 require"bqf".setup({
     auto_enable = true,
     preview = { win_height = 12, win_vheight = 12, delay_syntax = 80 },
-    filter = { fzf = { extra_opts = { '--bind', 'ctrl-o:toggle-all', '--prompt', '> ' } } }
+    filter = {
+        fzf = { action_for = { ['ctrl-s'] = 'split' } },
+        extra_opts = { '--bind', 'ctrl-o:toggle-all', '--prompt', '> ' }
+    }
 })
 
 vim.cmd([[
