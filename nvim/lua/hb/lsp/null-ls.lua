1d7af7b4674e83cb36b660b2c0c1b97761a435b9
liyong <hungrybirder@gmail.com>
2022-02-23 16:57:17 +0800

    config null-ls

--- nvim/lua/hb/lsp/null-ls.lua
+++ nvim/lua/hb/lsp/null-ls.lua
@@ -9,4 +9,15 @@ null_ls.setup({
         formatting.shfmt,
         formatting.yapf,
     },
+    on_attach = function(client)
+        if client.supports_method("textDocument/formatting") then
+            -- wrap in an augroup to prevent duplicate autocmds
+            vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+        end
+    end,
 })
