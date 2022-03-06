aec40acff3e2b33a6f33955063f79772268bcd95
liyong <hungrybirder@gmail.com>
2022-03-06 09:20:36 +0800

    Update telescope config

--- nvim/lua/hb/lsp/keymap.lua
+++ nvim/lua/hb/lsp/keymap.lua
@@ -19,7 +19,7 @@ M.setup_lsp_keymaps = function(client, bufnr)
     buf_set_keymap("n", "sgd", ":split | wincmd k | lua vim.lsp.buf.definition()<CR>", opts)
 
     buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    buf_set_keymap("n", "gl", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+    buf_set_keymap("n", "gl", "<cmd>lua vim.diagnostic.set_loclist()<CR>", opts)
     buf_set_keymap("n", "<space>i", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
     buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
     buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
