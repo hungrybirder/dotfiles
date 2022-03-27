bb43bf877a98f2aab5937e2c7c85c09d49a56740
liyong <hungrybirder@gmail.com>
2022-03-27 21:02:14 +0800

    Remove nvim-tree auto_close

--- nvim/lua/hb/nvim-tree.lua
+++ nvim/lua/hb/nvim-tree.lua
@@ -22,7 +22,6 @@ require("nvim-tree").setup({
     hijack_netrw = true,
     open_on_setup = false,
     ignore_ft_on_setup = {},
-    auto_close = true,
     open_on_tab = false,
     hijack_cursor = false,
     update_cwd = false,
