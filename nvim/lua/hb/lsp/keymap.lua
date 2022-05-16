3a63b6707f5046b242e047bad33a7b4f7d206ac0
liyong <hungrybirder@gmail.com>
2022-05-16 12:52:13 +0800

    fix lsp key mappings

--- nvim/lua/hb/lsp/keymap.lua
+++ nvim/lua/hb/lsp/keymap.lua
@@ -2,45 +2,37 @@
 local M = {}
 
 M.setup_lsp_keymaps = function(client, bufnr)
-    vim.api.nvim_buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
     local opts = { noremap = true, silent = true }
-    vim.api.nvim_buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 
-    vim.api.nvim_buf_set_keymap("n", "vgd", ":vsplit | wincmd h | lua vim.lsp.buf.definition()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "sgd", ":split | wincmd k | lua vim.lsp.buf.definition()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "vgd", ":vsplit | wincmd h | lua vim.lsp.buf.definition()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "sgd", ":split | wincmd k | lua vim.lsp.buf.definition()<CR>", opts)
 
-    vim.api.nvim_buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "gl", "<cmd>lua vim.diagnostic.set_loclist()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "<space>i", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap("v", "<space>ca", "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "<space>ca", "<cmd>Lspsaga code_action<CR>", opts)
-    vim.api.nvim_buf_set_keymap("v", "<space>ca", ":Lspsaga range_code_action<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "<space>cl", "<cmd>lua vim.lsp.codelens.run()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "<space>cr", "<cmd>lua vim.lsp.codelens.refresh()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-
-    vim.api.nvim_buf_set_keymap("n", "gic", "<cmd>lua vim.lsp.buf.incoming_calls()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "goc", "<cmd>lua vim.lsp.buf.outgoing_calls()<CR>", opts)
-
-    vim.api.nvim_buf_set_keymap("n", "gs", "<cmd>Lspsaga signature_help<CR>", opts)
-
-    vim.api.nvim_buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-
-    vim.api.nvim_buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-
-    vim.api.nvim_buf_set_keymap("n", "<space>h", '<cmd>lua require("lspsaga.provider").preview_definition()<CR>', opts)
-
-    -- TODO: learn workspace
-    vim.api.nvim_buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    vim.api.nvim_buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.set_loclist()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>i", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>ca", "<cmd>Lspsaga code_action<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "v", "<space>ca", ":Lspsaga range_code_action<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>cl", "<cmd>lua vim.lsp.codelens.run()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>cr", "<cmd>lua vim.lsp.codelens.refresh()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gic", "<cmd>lua vim.lsp.buf.incoming_calls()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "goc", "<cmd>lua vim.lsp.buf.outgoing_calls()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", "<cmd>Lspsaga signature_help<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>h", '<cmd>lua require("lspsaga.provider").preview_definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
     vim.api.nvim_buf_set_keymap(
+        bufnr,
         "n",
         "<space>wl",
         "<cmd>lua vim.pretty_print(vim.lsp.buf.list_workspace_folders())<CR>",
