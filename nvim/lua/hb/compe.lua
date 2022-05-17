d1a21c00856ee08d72adb8689fa3f2b691eb906b
liyong <hungrybirder@gmail.com>
2022-05-17 15:54:52 +0800

    set cmp sources

--- nvim/lua/hb/compe.lua
+++ nvim/lua/hb/compe.lua
@@ -41,12 +41,12 @@ cmp.setup({
     },
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        { name = "nvim_lsp_signature_help" },
-        { name = "nvim_lua" },
         { name = "luasnip" },
         { name = "buffer" },
+        { name = "nvim_lua" },
         { name = "path" },
         { name = "emoji" },
+        { name = "nvim_lsp_signature_help" },
     }),
     formatting = {
         format = require("lspkind").cmp_format({
