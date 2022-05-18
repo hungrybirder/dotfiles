310463efd34bd07fcd5602376b298e839570eb2b
liyong <hungrybirder@gmail.com>
2022-05-18 16:35:37 +0800

    Add two commands: EnableLspDiagnostic & DisableLspDiagnostic

--- nvim/lua/hb/lsp/init.lua
+++ nvim/lua/hb/lsp/init.lua
@@ -1,3 +1,6 @@
+vim.api.nvim_command("command! -nargs=0 DisableLspDiagnostic :lua vim.diagnostic.disable()<CR>")
+vim.api.nvim_command("command! -nargs=0 EnableLspDiagnostic :lua vim.diagnostic.enable()<CR>")
+
 require("hb/lsp/langservers")
 -- require("hb/lsp/jdtls")
 require("hb/lsp/null-ls")
