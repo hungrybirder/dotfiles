a1cbf2d3f6f8eac7c0381ef4dab1e6381cc1257b
liyong <hungrybirder@gmail.com>
2022-06-27 09:02:38 +0800

    Update compe.lua under_comparator

--- nvim/lua/hb/compe.lua
+++ nvim/lua/hb/compe.lua
@@ -12,11 +12,7 @@ local under_comparator = function(entry1, entry2)
     local _, entry2_under = entry2.completion_item.label:find("^_+")
     entry1_under = entry1_under or 0
     entry2_under = entry2_under or 0
-    if entry1_under > entry2_under then
-        return false
-    elseif entry1_under < entry2_under then
-        return true
-    end
+    return entry1_under < entry2_under
 end
 
 local cmp = require("cmp")
