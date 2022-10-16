6cba401ceece4e82ef1f817e55a757deded0f650
liyong <hungrybirder@gmail.com>
2022-10-16 10:29:44 +0800

    lsp: update_capabilities -> default_capabilities

--- nvim/lua/hb/lsp/langservers.lua
+++ nvim/lua/hb/lsp/langservers.lua
@@ -29,7 +29,7 @@ end
 
 local function make_lsp_client_capabilities()
     -- cmp_nvim_lsp take care of snippetSupport and resolveSupport
-    local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
     return capabilities
 end
 
