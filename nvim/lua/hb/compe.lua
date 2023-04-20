267511713d3996504117c86d5c11dbe3e8befe47
liyong <hungrybirder@gmail.com>
2023-04-20 22:05:09 +0800

    Format compe.lua and plugins.lua

--- nvim/lua/hb/compe.lua
+++ nvim/lua/hb/compe.lua
@@ -76,7 +76,6 @@ cmp.setup({
                 fallback()
             end
         end, { "i", "s" }),
-
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
--- nvim/lua/hb/plugins.lua
+++ nvim/lua/hb/plugins.lua
@@ -328,14 +328,14 @@ return packer.startup(function(use)
     use({
         "hrsh7th/nvim-cmp",
         requires = {
-            { "hrsh7th/cmp-buffer", after = "nvim-cmp" },
             "hrsh7th/cmp-nvim-lsp",
             "onsails/lspkind.nvim",
-            { "hrsh7th/cmp-nvim-lsp-signature-help", after = "nvim-cmp" },
-            { "hrsh7th/cmp-path", after = "nvim-cmp" },
-            { "hrsh7th/cmp-nvim-lua", after = "nvim-cmp" },
-            { "saadparwaiz1/cmp_luasnip", after = "nvim-cmp" },
-            { "hrsh7th/cmp-cmdline", after = "nvim-cmp" },
+            { "hrsh7th/cmp-buffer",                   after = "nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lsp-signature-help",  after = "nvim-cmp" },
+            { "hrsh7th/cmp-path",                     after = "nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lua",                 after = "nvim-cmp" },
+            { "saadparwaiz1/cmp_luasnip",             after = "nvim-cmp" },
+            { "hrsh7th/cmp-cmdline",                  after = "nvim-cmp" },
             { "hrsh7th/cmp-nvim-lsp-document-symbol", after = "nvim-cmp" },
             {
                 "petertriho/cmp-git",
