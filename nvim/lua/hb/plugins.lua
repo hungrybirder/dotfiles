aaed7bd96f8a73459c418d2fe44d5405f56d1f78
liyong <hungrybirder@gmail.com>
2023-05-06 14:57:01 +0800

    Update luasnip dep

--- nvim/lua/hb/plugins.lua
+++ nvim/lua/hb/plugins.lua
@@ -364,10 +364,12 @@ require("lazy").setup({
     -- snippets
     {
         "L3MON4D3/LuaSnip",
-        -- install jsregexp (optional!:).
         build = "make install_jsregexp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
     },
-    { "rafamadriz/friendly-snippets" },
 
     -- auto completion
     {
