9af9626114b4bb757edb4f058daa06d6ceeea37a
liyong <hungrybirder@gmail.com>
2023-05-15 09:22:16 +0800

    Update dap-go config

--- nvim/lua/hb/dap/dap-go.lua
+++ nvim/lua/hb/dap/dap-go.lua
@@ -1,1 +1,13 @@
-require("dap-go").setup()
+require("dap-go").setup({
+    dap_configurations = {
+        {
+            type = "go",
+            name = "Attach remote",
+            mode = "remote",
+            request = "attach",
+        },
+    },
+    delve = {
+        port = "38697",
+    },
+})
