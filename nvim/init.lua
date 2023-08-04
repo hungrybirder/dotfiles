2cf87330a679fb35b9993565e8b3e800aebb629f
liyong <hungrybirder@gmail.com>
2023-08-04 11:03:23 +0800

    Add comment

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,10 @@
 P = vim.print
 
 require("hb/options")
+require("hb/keymaps")
 require("hb/autocmds")
 require("hb/plugins")
 require("hb/colorscheme")
-require("hb/misc")
-require("hb/keymaps")
 require("hb/treesitter")
 require("hb/lsp")
+require("hb/misc") -- misc will be removed.
