7df90e980eeab44e6d078daf14fddc47afb6da2e
liyong <hungrybirder@gmail.com>
2023-08-04 15:21:33 +0800

    Update json.lua

--- nvim/after/ftplugin/json.lua
+++ nvim/after/ftplugin/json.lua
@@ -3,7 +3,7 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = true
 vim.opt_local.smarttab = true
 
-vim.keympap.set("n", "<left>", "<cmd>lua require("jvim").to_parent()<CR>")
-vim.keympap.set("n", "<right>", "<cmd>lua require("jvim").descend()<CR>")
-vim.keympap.set("n", "<up>", "<cmd>lua require("jvim").prev_sibling()<CR>")
-vim.keympap.set("n", "<down>", "<cmd>lua require("jvim").next_sibling()<CR>")
+vim.keympap.set("n", "<left>", "<cmd>lua require('jvim').to_parent()<CR>")
+vim.keympap.set("n", "<right>", "<cmd>lua require('jvim').descend()<CR>")
+vim.keympap.set("n", "<up>", "<cmd>lua require('jvim').prev_sibling()<CR>")
+vim.keympap.set("n", "<down>", "<cmd>lua require('jvim').next_sibling()<CR>")
