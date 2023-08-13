489831f5cbdd79fbd29a63040277f70ac5a22199
liyong <hungrybirder@gmail.com>
2023-08-13 19:33:35 +0800

    Update lspsaga config

--- nvim/lua/hb/plugins.lua
+++ nvim/lua/hb/plugins.lua
@@ -576,14 +576,25 @@ require("lazy").setup({
             vim.keymap.set("n", "<leader>v", "<cmd>Lspsaga outline<CR>")
             require("lspsaga").setup({
                 diagnostic = {
+                    show_code_action = true,
+                    show_layout = "float",
+                    show_normal_height = 10,
+                    jump_num_shortcut = true,
+                    max_width = 0.8,
+                    max_height = 0.6,
+                    max_show_width = 0.9,
+                    max_show_height = 0.6,
+                    text_hl_follow = true,
+                    border_follow = true,
+                    extend_relatedInformation = false,
+                    diagnostic_only_current = false,
                     keys = {
                         exec_action = "o",
-                        quit = { "q", "<ESC>" },
+                        quit = "q",
                         toggle_or_jump = "<CR>",
                         quit_in_show = { "q", "<ESC>" },
                     },
                 },
-
                 code_action = {
                     keys = {
                         quit = { "q", "<ESC>" },
