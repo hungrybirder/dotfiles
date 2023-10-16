a1925ca01467902823fe65ba987e4cc0da0c2088
liyong <hungrybirder@gmail.com>
2023-10-16 14:50:58 +0800

    Delete nouseful codes

--- nvim/lua/hb/plugins.lua
+++ nvim/lua/hb/plugins.lua
@@ -190,8 +190,6 @@ require("lazy").setup({
     {
         "farmergreg/vim-lastplace",
         config = function()
-            -- vim.g.lastplace_ignore = { "gitcommit", "gitrebase", "svn", "hgcommit" }
-            -- vim.g.lastplace_ignore_buftype = { "quickfix", "nofile", "help" }
             vim.g.lastplace_ignore = "gitcommit, gitrebase, svn, hgcommit"
             vim.g.lastplace_ignore_buftype = "quickfix, nofile, help"
             vim.g.lastplace_open_folds = 1
