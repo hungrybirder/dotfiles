bebacba062855b117908814354af1a4b16e85e7c
liyong <hungrybirder@gmail.com>
2023-10-18 17:23:37 +0800

    Remove navic refs

--- nvim/lua/hb/lsp/langservers.lua
+++ nvim/lua/hb/lsp/langservers.lua
@@ -92,8 +92,6 @@ end
 -- if you want to set up formatting on save, you can use this as a callback
 local lsp_fmt_augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
--- local navic = require("nvim-navic")
-
 local function lsp_on_attach(client, bufnr)
     require("hb/lsp/keymap").setup_lsp_keymaps(client, bufnr)
     require("lspkind").init({})
@@ -108,9 +106,6 @@ local function lsp_on_attach(client, bufnr)
             end,
         })
     end
-    -- if client.server_capabilities.documentSymbolProvider then
-    --     navic.attach(client, bufnr)
-    -- end
 end
 
 local function make_lsp_client_capabilities()
@@ -325,9 +320,6 @@ setup_jdtls = function()
         })
         require("jdtls.dap").setup_dap_main_class_configs()
         vim.lsp.codelens.refresh()
-        if client.server_capabilities.documentSymbolProvider then
-            navic.attach(client, bufnr)
-        end
     end
 
     local root_markers = { "gradlew", "pom.xml" }
