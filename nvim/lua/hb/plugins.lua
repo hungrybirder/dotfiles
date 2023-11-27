1e18be2aebfc6bfbbcfa71a85bc0b7a03be4a8ac
liyong <hungrybirder@gmail.com>
2023-11-27 10:35:33 +0800

    Update injectme.nvim config

--- nvim/lua/hb/plugins.lua
+++ nvim/lua/hb/plugins.lua
@@ -951,6 +951,24 @@ require("lazy").setup({
         },
         -- This is for lazy load and more performance on startup only
         cmd = { "InjectmeToggle", "InjectmeSave", "InjectmeInfo", "InjectmeLeave" },
+        config = function()
+            require("injectme").setup({
+                -- mode = "standard",
+                mode = "standard",
+                -- "all"/ "none" if all/no injections should be activated on startup
+                --    When you use, lazy loading, call :InjectemeInfo to activate
+                -- "standard", if no injections should be changed from standard settings in
+                --    the runtime directory, i.e. ~/.config/nvim/queries/<language>/injections.scm
+                reload_all_buffers = true,
+                -- after toggling an injection, all buffers are reloaded to reset treesitter
+                -- you can set this to false, and avoid that the plugin asks you to save buffers
+                -- before changing an injection
+                reset_treesitter = true,
+                -- after toggling an injections, the treesitter parser is reset
+                -- Currently, this does nothing, see this discussion on github
+                -- https://github.com/nvim-treesitter/nvim-treesitter/discussions/5684
+            })
+        end,
     },
     -- best folder plugin nvim-ufo
     {
