db2f2b66733d10fc711954074ce2051a04af35b0
liyong <hungrybirder@gmail.com>
2023-11-27 10:23:31 +0800

    Add queries to .gitignore

--- nvim/queries/html/injections.scm
+++
@@ -1,9 +0,0 @@
-;extends
-;pythoncode
-        (element
-          (start_tag
-            (attribute
-              (quoted_attribute_value
-                ((attribute_value) @_idd (#eq? @_idd "python")))))
-          ((text) @injection.content (#set! injection.language "python")))
-      
