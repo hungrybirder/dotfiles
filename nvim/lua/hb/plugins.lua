5285144c756b0fd3f488b5df8cdfa38d0c4d3d13
liyong <hungrybirder@gmail.com>
2024-05-20 08:53:59 +0800

    use master branch of neogit

--- nvim/lua/hb/plugins.lua
+++ nvim/lua/hb/plugins.lua
@@ -333,7 +333,6 @@ require("lazy").setup({
     },
     {
         "NeogitOrg/neogit",
-        branch = "nightly",
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-telescope/telescope.nvim",
