60f033cc7751e4331214622117b8b443c66612e3
liyong <hungrybirder@gmail.com>
2024-10-25 15:13:33 +0800

    Update to-do config

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -477,9 +477,7 @@ return {
     {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("todo-comments").setup({})
-        end,
+        opts = {},
     },
 
     {
