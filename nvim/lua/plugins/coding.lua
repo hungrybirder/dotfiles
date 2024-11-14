2c705823bfa203c5077072144a23c51562c18c14
liyong <hungrybirder@gmail.com>
2024-11-14 14:24:22 +0800

    Try maven.nvim and gradle.nvim

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -593,4 +593,42 @@ return {
             })
         end,
     },
+
+    -- java maven and gradle
+    {
+        "oclay1st/maven.nvim",
+        cmd = { "Maven", "MavenInit", "MavenExec" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+        },
+        opts = {}, -- options, see default configuration
+        keys = {
+            {
+                "<Leader>M",
+                function()
+                    require("maven").toggle_projects_view()
+                end,
+                desc = "Maven",
+            },
+        },
+    },
+    {
+        "oclay1st/gradle.nvim",
+        cmd = { "Gradle", "GradleExec", "GradleInit" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+        },
+        opts = {}, -- options, see default configuration
+        keys = {
+            {
+                "<Leader>G",
+                function()
+                    require("gradle").toggle_projects_view()
+                end,
+                desc = "Gradle",
+            },
+        },
+    },
 }
