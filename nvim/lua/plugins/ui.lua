47159c5eed4fb56b5625d5c4135f0f5b112208a6
liyong <hungrybirder@gmail.com>
2025-09-29 20:51:54 +0800

    Remove zenmode

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -45,18 +45,18 @@ return {
     },
 
     -- free writing
-    {
-        "folke/zen-mode.nvim",
-        cmd = "ZenMode",
-        opts = {
-            plugins = {
-                gitsigns = true,
-                tmux = true,
-                kitty = { enabled = false, font = "+2" },
-            },
-        },
-        keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-    },
+    -- {
+    --     "folke/zen-mode.nvim",
+    --     cmd = "ZenMode",
+    --     opts = {
+    --         plugins = {
+    --             gitsigns = true,
+    --             tmux = true,
+    --             kitty = { enabled = false, font = "+2" },
+    --         },
+    --     },
+    --     keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+    -- },
     {
         "nvim-lualine/lualine.nvim",
         lazy = false,
