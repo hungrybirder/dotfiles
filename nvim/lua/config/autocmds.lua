d099238cad040de6bf7c7c80698152b71c92a91f
liyong <hungrybirder@gmail.com>
2026-04-03 14:47:32 +0800

    Use LspProgress instead of fidget.nvim and use ui2

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -47,6 +47,20 @@ vim.api.nvim_create_autocmd("LspAttach", {
     desc = "LSP Attach Post",
 })
 
+vim.api.nvim_create_autocmd("LspProgress", {
+    callback = function(ev)
+        local value = ev.data.params.value
+        vim.api.nvim_echo({ { value.message or "done" } }, false, {
+            id = "lsp." .. ev.data.client_id,
+            kind = "progress",
+            source = "vim.lsp",
+            title = value.title,
+            status = value.kind ~= "end" and "running" or "success",
+            percent = value.percentage,
+        })
+    end,
+})
+
 -- from neovim v0.12.0 LspInfo/LspLog/LspRestart are deprecated
 vim.api.nvim_create_user_command("LspInfo", "checkhealth vim.lsp", {
     desc = "Show LSP Info",
@@ -63,4 +77,29 @@ end, {
 
 vim.api.nvim_create_user_command("LspRestart", "lsp restart", {
     desc = "Restart LSP",
+})
+
+-- With UI2, there is not more annoying “Press Enter” prompt after you run a command.
+require("vim._core.ui2").enable({
+    enable = true,
+    msg = { -- Options related to the message module.
+        ---@type 'cmd'|'msg' Default message target, either in the
+        ---cmdline or in a separate ephemeral message window.
+        ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
+        ---or table mapping |ui-messages| kinds and triggers to a target.
+        targets = "cmd",
+        cmd = { -- Options related to messages in the cmdline window.
+            height = 0.5, -- Maximum height while expanded for messages beyond 'cmdheight'.
+        },
+        dialog = { -- Options related to dialog window.
+            height = 0.5, -- Maximum height.
+        },
+        msg = { -- Options related to msg window.
+            height = 0.5, -- Maximum height.
+            timeout = 4000, -- Time a message is visible in the message window.
+        },
+        pager = { -- Options related to message window.
+            height = 0.5, -- Maximum height.
+        },
+    },
 })
