05abea640e61cca2508e3d54069a4a11db68e77a
liyong <hungrybirder@gmail.com>
2026-04-05 12:42:22 +0800

    Try opencode.nvim

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,11 +1,33 @@
 return {
+    -- {
+    --     "greggh/claude-code.nvim",
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim", -- Required for git operations
+    --     },
+    --     config = function()
+    --         require("claude-code").setup()
+    --     end,
+    -- },
     {
-        "greggh/claude-code.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim", -- Required for git operations
-        },
+        "sudo-tee/opencode.nvim",
         config = function()
-            require("claude-code").setup()
+            require("opencode").setup({})
         end,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "MeanderingProgrammer/render-markdown.nvim",
+                opts = {
+                    anti_conceal = { enabled = false },
+                    file_types = { "markdown", "opencode_output" },
+                },
+                ft = { "markdown", "Avante", "copilot-chat", "opencode_output" },
+            },
+            -- Optional, for file mentions and commands completion
+            "hrsh7th/nvim-cmp",
+
+            -- Optional, for file mentions picker
+            "nvim-telescope/telescope.nvim",
+        },
     },
 }
--- nvim/lua/util/lsp.lua
+++ nvim/lua/util/lsp.lua
@@ -177,19 +177,21 @@ function M.lsp_on_attach_post(client, bufnr)
     })
 
     -- config providers
-    if client.server_capabilities.documentHighlightProvider then
-        local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-            group = highlight_augroup,
-            buffer = bufnr,
-            callback = vim.lsp.buf.document_highlight,
-        })
-        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-            group = highlight_augroup,
-            buffer = bufnr,
-            callback = vim.lsp.buf.clear_references,
-        })
-    end
+
+    -- Temp disable document highlight for using opencode
+    -- if client.server_capabilities.documentHighlightProvider then
+    --     local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+    --     vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+    --         group = highlight_augroup,
+    --         buffer = bufnr,
+    --         callback = vim.lsp.buf.document_highlight,
+    --     })
+    --     vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+    --         group = highlight_augroup,
+    --         buffer = bufnr,
+    --         callback = vim.lsp.buf.clear_references,
+    --     })
+    -- end
 
     if client.server_capabilities.inlayHintProvider then
         vim.lsp.inlay_hint.enable(true, {
