aefe1ff38a8e6587f75eacfbc9ddde5ed8c74724
liyong <hungrybirder@gmail.com>
2026-04-16 02:30:35 +0800

    Remove opencode and config render-markdown

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,26 +1,26 @@
 return {
-    {
-        "sudo-tee/opencode.nvim",
-        config = function()
-            require("opencode").setup({})
-        end,
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            {
-                "MeanderingProgrammer/render-markdown.nvim",
-                opts = {
-                    anti_conceal = { enabled = false },
-                    file_types = { "markdown", "opencode_output" },
-                },
-                ft = { "markdown", "Avante", "copilot-chat", "opencode_output" },
-            },
-            -- Optional, for file mentions and commands completion
-            "hrsh7th/nvim-cmp",
-
-            -- Optional, for file mentions picker
-            "nvim-telescope/telescope.nvim",
-        },
-    },
+    -- {
+    --     "sudo-tee/opencode.nvim",
+    --     config = function()
+    --         require("opencode").setup({})
+    --     end,
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim",
+    --         {
+    --             "MeanderingProgrammer/render-markdown.nvim",
+    --             opts = {
+    --                 anti_conceal = { enabled = false },
+    --                 file_types = { "markdown", "opencode_output" },
+    --             },
+    --             ft = { "markdown", "Avante", "copilot-chat", "opencode_output" },
+    --         },
+    --         -- Optional, for file mentions and commands completion
+    --         "hrsh7th/nvim-cmp",
+    --
+    --         -- Optional, for file mentions picker
+    --         "nvim-telescope/telescope.nvim",
+    --     },
+    -- },
     {
         "Exafunction/windsurf.nvim",
         dependencies = {
